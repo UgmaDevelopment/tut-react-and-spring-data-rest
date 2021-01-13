6ff87a0a8619291fff32357022b340271d4de13d
Bradley Turek <UgmaDevelopment@gmail.com>
2021-01-13 10:19:10 -0700

    Fixes `EmployeeList` spacing

--- basic/src/main/js/app.js
+++ basic/src/main/js/app.js
@@ -29,24 +29,24 @@ class App extends React.Component { // <1>
 // end::app[]
 
 // tag::employee-list[]
-class EmployeeList extends React.Component{
-	render() {
-		const employees = this.props.employees.map(employee =>
-			<Employee key={employee._links.self.href} employee={employee}/>
-		);
-		return (
-			<table>
-				<tbody>
-					<tr>
-						<th>First Name</th>
-						<th>Last Name</th>
-						<th>Description</th>
-					</tr>
-					{employees}
-				</tbody>
-			</table>
-		)
-	}
+class EmployeeList extends React.Component {
+    render() {
+        const employees = this.props.employees.map(employee =>
+            <Employee key={employee._links.self.href} employee={employee}/>
+        );
+        return (
+            <table>
+                <tbody>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Description</th>
+                </tr>
+                {employees}
+                </tbody>
+            </table>
+        )
+    }
 }
 // end::employee-list[]
 
